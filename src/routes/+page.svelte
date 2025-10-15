176db039617dc46034a65a58c9a2ea7b4127bb65
Andrew Wang <andrewwangpa@gmail.com>
2025-10-14 22:43:04 -0400

    Installed Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import {
+		MapLibre,
+		Marker,
+		NavigationControl,
+		ScaleControl,
+		GlobeControl,
+		GeolocateControl
+	} from 'svelte-maplibre-gl';
+
+	let lnglat: [number, number] = [-73.679711, 42.729878];
+</script>
+
+<MapLibre
+	class="h-[100vh] min-h-[300px]"
+	style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+	zoom={16}
+	center={{ lng: -73.679711, lat: 42.729878 }}
+>
+	<NavigationControl />
+	<ScaleControl />
+
+	<Marker bind:lnglat />
+    
+	<GeolocateControl
+		position="top-left"
+		positionOptions={{ enableHighAccuracy: true }}
+		trackUserLocation={true}
+		showAccuracyCircle={false}
+	/>
+</MapLibre>
