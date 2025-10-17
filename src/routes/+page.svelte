a3ff7d41b487a44e621bd3de845c2e9ab92f2ebb
Andrew Wang <andrewwangpa@gmail.com>
2025-10-17 13:36:41 -0400

    Added Marker for Sage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,23 @@
 		Marker,
 		NavigationControl,
 		ScaleControl,
-		GlobeControl,
-		GeolocateControl
+		GeolocateControl,
+		Popup
 	} from 'svelte-maplibre-gl';
 
-	let lnglat: [number, number] = [-73.679711, 42.729878];
+	let offset = $state(24);
+
+	let offsets: maplibregl.Offset = $derived({
+		top: [0, offset],
+		bottom: [0, -offset],
+		left: [offset + 12, 0],
+		right: [-offset - 12, 0],
+		center: [0, 0],
+		'top-left': [offset, offset],
+		'top-right': [-offset, offset],
+		'bottom-left': [offset, -offset],
+		'bottom-right': [-offset, -offset]
+	});
 </script>
 
 <MapLibre
@@ -20,7 +32,12 @@
 	<NavigationControl />
 	<ScaleControl />
 
-	<Marker bind:lnglat />
+	<Marker lnglat={[-73.679711, 42.729878]} />
+	<Marker lnglat={[-73.681651, 42.730809]}> /* Sage */
+		<Popup class="text-black"  offset={offsets}>
+			<span class="text-lg">hello</span>
+		</Popup> 
+	</Marker>
     
 	<GeolocateControl
 		position="top-left"
